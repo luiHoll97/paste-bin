f1509b847d77366ec476a45b32a008af5ee0f6ed
Lui H <tech.luiholl@gmail.com>
2022-09-07 16:15:17 +0200

    added get 1, edit 1, delete 1 (paste) to routes

--- server.ts
+++ server.ts
@@ -27,7 +27,12 @@ const client = new Client(dbConfig);
 client.connect();
 
 app.get("/pastes", async (req, res) => {
-  const dbres = await client.query('select * from pastebin');
+
+  // hey database (client - has connection string which is unique to DB and a ssl key for access), 
+  // we're gunna pass you a query(some SQL)
+  const dbres = await client.query('select * from pastebin order by paste_id asc');
+  // when you've done that, can you give me back the results of the query and only give me the rows
+  // from the database?
   res.json(dbres.rows);
 });
 
@@ -36,6 +41,58 @@ app.get("/pastes", async (req, res) => {
   res.json(dbres.rows);
 });
 */
+
+// get a todo by id
+
+app.get("/pastes/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const getPasteById = await client.query(
+      "select * from pastebin where paste_id = $1", [id]
+    )
+    res.json(getPasteById.rows)
+  } catch (error) {
+    console.log(error.message)
+
+  }
+})
+
+// edit an existing paste
+
+app.put("/pastes/:id", async (req, res) => {
+  try {
+    const { id } = req.params // get the id to pass to SQL
+    const { snippet } = req.body // get the values for snippet/owner to pass to SQL
+    const editPaste = await client.query(
+      "update pastebin set snippet = $1 where paste_id = $2 returning *", [snippet, id]
+    )
+    res.json(editPaste.rows)
+    // update todo set description = $1 where todo_id = $2", [description, id]
+  } catch (error) {
+    console.log(error.message)
+
+  }
+})
+
+// delete a paste
+
+app.delete("/pastes/:id", async (req, res) => {
+  try {
+    const { id } = req.params // get the id to pass to sequal
+    const deletePaste = await client.query(
+      "delete from pastebin where paste_id = $1 returning *", [id]
+    )
+    res.send(`paste successfully deleted`)
+    // res.json(deletePaste.rows) **NOTE: only sends 1 response - this is unreadable 
+  } catch (error) {
+    console.log(error.message)
+
+  }
+})
+
+
+// post an id
+
 app.post("/pastes", async (req, res) => {
   try {
     const { snippet, owner } = req.body; // this takes JSON data to use
@@ -53,7 +110,6 @@ app.post("/pastes", async (req, res) => {
     console.log("didnt work mate");
   }
 })
-
 
 
 //Start the server on the given port
